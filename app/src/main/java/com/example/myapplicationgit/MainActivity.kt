91206e64d573d9c233261b4a33846ce32a832aab
=Maria Nava <=>
2020-12-16 17:06:01 -0300

    second commit

--- app/src/main/java/com/example/myapplicationgit/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationgit/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(tag: "TAG", msg: "Test 1")
     }
 }
