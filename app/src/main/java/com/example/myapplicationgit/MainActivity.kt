d1830c1b489ee92bf1b0277c5fe5c5a6b118105b
=Maria Nava <=>
2020-12-16 17:10:51 -0300

    second commit

--- app/src/main/java/com/example/myapplicationgit/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationgit/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(tag: "TAG", msg: "Test 1")
+        Log.d(tag: "TAG", msg: "Test 2")
     }
 }
